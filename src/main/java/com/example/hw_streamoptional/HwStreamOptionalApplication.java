1a0934adb016161ad0136f1b97ec60fe9ed52119
дом <amir/shakirov.2015@mail.ru>
2023-02-16 12:51:19 +0400

    Initial commit

---
+++ src/main/java/com/example/hw_streamoptional/HwStreamOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw_streamoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwStreamOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwStreamOptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw_streamoptional/HwStreamOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_streamoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwStreamOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
